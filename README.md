de93489f4c3816cf2ff87428731b4fb7eb56df09
Mathijs de Bruin <mathijs@mathijsfietst.nl>
2024-05-05 13:17:03 +0100

    Remove DMX stuff.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# dmx-fastled
-Experimentally driving dirt cheap DMX fixtures with FastLED. Can haz rainbows, colour calibration and candle effect.
+# esp32-neopixel-candles
+
+Emulate candles on 'neopixel' (WS2812B) type of lights on ESP32 mycontrollers.
+
+## Features
+* Async wifi web config.
+
+## Stack
+* WS2812B
+* ESP32
+* [Candles](https://github.com/dokterbob/Candle)
+* [PlatformIO](https://platformio.org/)
+* [FastLED](https://fastled.io/)
+* [IotWebConf](https://github.com/prampec/IotWebConf)
--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,27 @@
 #include <HTTPUpdateServer.h>
 
 // -- Initial name of the Thing. Used e.g. as SSID of the own Access Point.
-const char thingName[] = "Jemoeder";
+const char thingName[] = "Candles";
 
 // -- Initial password to connect to the Thing, when it creates an own Access Point.
-const char wifiInitialApPassword[] = "everythingmustgo";
+const char wifiInitialApPassword[] = "candles";
 
 #define ONBOARD_LED 2
-#define NUM_LEDS 12
-#define CONFIG_VERSION "dem4"
+
+#define NUM_STRIPS 5
+#define NUM_LEDS_PER_STRIP 64
+#define NUM_LEDS NUM_STRIPS * NUM_LEDS_PER_STRIP
+
+// Use 3 LED's to make white
+#define HUE_DEGREES 360/3
+
+// How long to wait before increasing hue
+#define HUE_DELAY 100
+
+// How long to wait between candle frames
+#define CANDLE_DELAY 16
+
+#define CONFIG_VERSION "01"
 
 // -- Method declarations.
 void handleRoot();
@@ -54,60 +67,76 @@ iotwebconf::IntTParameter<uint8_t> brightnessParam =
   build();
 
 // Set these to your desired credentials.
-const char *ssid = "Jemoeder";
-const char *password = "allmustgo";
+const char *ssid = "Candles";
+const char *password = "candles";
 
 CRGB leds[NUM_LEDS];
 candle::Candle candles[NUM_LEDS];
 candle::Candle saturation[NUM_LEDS];
+
+void setupFastLED() {
+  // https://www.reddit.com/r/FastLED/comments/gpu1fv/best_pins_to_use_with_esp32/
+  FastLED.addLeds<NEOPIXEL, 13>(leds, 0 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 14>(leds, 1 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 15>(leds, 2 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 18>(leds, 3 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, 19>(leds, 4 * NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
+
+  FastLED.setCorrection(TypicalPixelString);
+  FastLED.setTemperature(Candle);
+
+  // Reset LED values
+  FastLED.clear();
+  FastLED.show();
+}
+
+void setupIotWebConf() {
+  // -- Define how to handle updateServer calls.
+  iotWebConf.setupUpdateServer(
+    [](const char* updatePath) { httpUpdater.setup(&server, updatePath); },
+    [](const char* userName, char* password) {
+      httpUpdater.updateCredentials(userName, password);
+    }
+  );
+
+  group1.addItem(&darknessParam);
+  group1.addItem(&candleParam);
+  group1.addItem(&rainbowParam);
+  group1.addItem(&brightnessParam);
+  iotWebConf.addParameterGroup(&group1);
+  iotWebConf.setConfigSavedCallback(&configSaved);
+
+  // -- Initializing the configuration.
+  iotWebConf.init();
+
+  // -- Set up required URL handlers on the web server.
+  server.on("/", handleRoot);
+  server.on("/config", []{ iotWebConf.handleConfig(); });
+  server.onNotFound([](){ iotWebConf.handleNotFound(); });
+}
+
+void setupCandles() {
+  for (size_t i=0; i<NUM_LEDS; i++) {
+      // Initialize candles
+      candles[i].init(96, 255, 2, 96);
+
+      // Initialize random saturation
+      saturation[i].init(112, 255, 20, 62);
+
+  }
+}
 
 void setup() {
     Serial.begin(9600);
     Serial.println();
     Serial.println("Starting up...");
 
-    // -- Define how to handle updateServer calls.
-    iotWebConf.setupUpdateServer(
-      [](const char* updatePath) { httpUpdater.setup(&server, updatePath); },
-      [](const char* userName, char* password) {
-        httpUpdater.updateCredentials(userName, password);
-      }
-    );
+    setupIotWebConf();
+    setupFastLED();
+    setupCandles();
 
-    group1.addItem(&darknessParam);
-    group1.addItem(&candleParam);
-    group1.addItem(&rainbowParam);
-    group1.addItem(&brightnessParam);
-    iotWebConf.addParameterGroup(&group1);
-    iotWebConf.setConfigSavedCallback(&configSaved);
-
-    // -- Initializing the configuration.
-    iotWebConf.init();
-
-    // -- Set up required URL handlers on the web server.
-    server.on("/", handleRoot);
-    server.on("/config", []{ iotWebConf.handleConfig(); });
-    server.onNotFound([](){ iotWebConf.handleNotFound(); });
-
+    // Signal that we're alive!
     pinMode(ONBOARD_LED, OUTPUT);
-
-    FastLED.addLeds<ESPDMX>(leds, NUM_LEDS);
-    FastLED.setCorrection(TypicalPixelString);
-    FastLED.setTemperature(Candle);
-    //  // THIS WORKS SO FUCKING WELL!!!
-
-    for (size_t i=0; i<NUM_LEDS; i++) {
-        // Reset LED values
-        leds[i] = CRGB::Black;
-
-        // Initialize candles
-        candles[i].init(96, 255, 2, 96);
-
-        // Initialize random saturation
-        saturation[i].init(112, 255, 20, 62);
-
-    }
-
     digitalWrite(ONBOARD_LED, HIGH);
 
     Serial.println("Ready.");
@@ -120,17 +149,13 @@ bool beARainbow = true;
 
 void loop()
 {
-    EVERY_N_MILLIS(16) {
+    EVERY_N_MILLIS(CANDLE_DELAY) {
       for (size_t i=0; i<NUM_LEDS; i++) {
-        if (beARainbow) {
-          leds[i] = CHSV(
-            add8(hue, mul8(i, 120)),
-            beACandle ? saturation[i].get_next_brightness() : 255,
-            beACandle ? candles[i].get_next_brightness() : 255
-          );
-        } else {
-          leds[i] = CRGB::White;
-        }
+        leds[i] = CHSV(
+          beARainbow ? add8(hue, mul8(i, HUE_DEGREES)) : 0,
+          beACandle ? saturation[i].get_next_brightness() : 255,
+          beACandle ? candles[i].get_next_brightness() : 255
+        );
       }
     }
 
@@ -138,7 +163,7 @@ void loop()
 
     iotWebConf.doLoop();
 
-    EVERY_N_MILLIS(150) {
+    EVERY_N_MILLIS(HUE_DELAY) {
       hue++;
     }
 }
@@ -155,10 +180,10 @@ void handleRoot()
     return;
   }
   String s = "<!DOCTYPE html><html lang=\"en\"><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1, user-scalable=no\"/>";
-  s += "<title>Je moeder (is the best DMX controller)</title>";
+  s += "<title>Je moeder (is the best light controller)</title>";
   s += "<style>.de{background-color:#ffaaaa;} .em{font-size:0.8em;color:#bb0000;padding-bottom:0px;} .c{text-align: center;} div,input,select{padding:5px;font-size:1em;} input{width:95%;} select{width:100%} input[type=checkbox]{width:auto;scale:1.5;margin:10px;} body{text-align: center;font-family:verdana;} button{border:0;border-radius:0.3rem;background-color:#16A1E7;color:#fff;line-height:2.4rem;font-size:1.2rem;width:100%;} fieldset{border-radius:0.3rem;margin: 0px;}</style>";
   s += "</head><body>";
-  s += "<h1>Je moeder</h1><h2>(is the best DMX controller)</h2>";
+  s += "<h1>Je moeder</h1><h2>(is the best light controller)</h2>";
 
   // Current config
   s += "<h3>Current config:<h3><ul>";
